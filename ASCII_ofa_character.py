8c411658676c928bf11df5f87ad641fdd17f2082
Joseph Kakai <josephngalu96@gmail.com>
2023-02-05 23:19:03 -0800

    Updated

--- ASCII_ofa_character.py
+++ ASCII_ofa_character.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-""" 
+"""
 This module prints the ASCII value of a character
 """
+
+
 def main():
     """
     The function that prints the ASCII value of a character
     """
+
     a = chr(input("Please key in your character to print its valu"))
     print("The ASCII value of {} is: {}".format(a, ord(a)))
+
 
 main()
